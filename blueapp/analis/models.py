3ffd429097cf0769fcd5b156b5e9b2976f62835e
aseazul <angelsanchez@azulweb.net>
2023-06-23 13:46:20 -0600

    Creación de módulos Sitios Web, Publicación y Engagement

--- blueapp/analis/models.py
+++ blueapp/analis/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class SitioWeb(models.Model):
+    sitio_web_id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    feed_url = models.CharField(max_length=255)
+
+class Publicacion(models.Model):
+    publicacion_id = models.IntegerField(primary_key=True)
+    sitio_web = models.ForeignKey(SitioWeb, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    imagen_portada = models.CharField(max_length=255)
+    extracto = models.CharField(max_length=250)
+
+class Engagement(models.Model):
+    engagement_id = models.IntegerField(primary_key=True)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    reacciones = models.IntegerField()
+    comentarios = models.IntegerField()
+    veces_compartidas = models.IntegerField()
+    comentarios_sitios_web = models.IntegerField()
+    total_engagement = models.IntegerField()
